3a9c9012e0f439133f11605c9329399495cdb581
Deborah.j.Palavecino <77463273+Debi-Palavecino@users.noreply.github.com>
2022-05-10 16:00:16 -0300

    no toma los estilos en linea o css de link

--- src/componentes/Nav.js
+++ src/componentes/Nav.js
@@ -1,21 +1,22 @@
+import { Link } from "react-router-dom"
 import "../styles/NavStyle.css"
 
 const Nav = ({handleClickAbrirModal})=>{
     return(
         <div className="Container">
-            <div className="boxConteiner">
-                <div className="logoSalta">
+            <div className="logoSalta">
                     <img alt="Logo De Salta" src="http://turismosalta.gov.ar/images/logo-turismo-salta.png"/>
-                </div>
-                <div className="box">
-                    <h2>Places</h2>
-                </div>
-                <div className="box">
-                    <h2>What to do</h2>
-                </div>
-                <div onClick={handleClickAbrirModal} className="box">
-                    <h2>History</h2>
-                </div>
+            </div>
+            <div className="boxConteiner">
+                <button className="box">
+                    <Link className="link" to={"/Lugares"}>Places</Link>
+                </button>
+                <button className="box">
+                    <Link to={"/QueHacer"}>What to do</Link>
+                </button>
+                <button onClick={handleClickAbrirModal} className="box">
+                    <Link Style={{fontWeight:"bold",color:"rgb(75,12,7)",}} to={"/Historia"}>History</Link>
+                </button>
             </div>
         </div>
     )

b493cbbe16b0832e77246019941c48218cff0392
Deborah.j.Palavecino <77463273+Debi-Palavecino@users.noreply.github.com>
2022-05-16 22:03:11 -0300

    parrafo creado

---
+++ src/componentes/Home.js
@@ -0,0 +1,19 @@
+import Carrousel from "./Carrousel"
+
+import Nav from "./Nav";
+const Home =()=>{
+    return(
+        <div>
+            <Nav/>
+            <Carrousel/>
+            <div>
+                <h1>
+                    Hola
+                </h1>
+                <p> lorem impus....</p>
+            </div>
+            
+        </div>
+    )
+}
+export default Home
